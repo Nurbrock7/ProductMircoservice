38eea179fb8ce0f14765cff96ec6ba0cd45ff023
Moegamat-Nur Brock <Nurbrock7@gmail.com>
2022-01-20 10:53:23 +0200

    Added Models Added DBcontext

---
+++ ProductMicroservice/ProductMicroservice/DbContexts/ProductContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductMicroservice.Models;
+using ProductMicroservice.Models.Product;
+
+namespace ProductMicroservice.DBContexts
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //ModelCreating is a method via which the master data could be seeded to the database.
+        //So, add the OnModelCreating method and add some sample categories that will be added to the database initially into the category table when the database is created.
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category
+                {
+                    Id = 1,
+                    Name = "Electronics",
+                    Description = "Electronic Items",
+                },
+                new Category
+                {
+                    Id = 2,
+                    Name = "Clothes",
+                    Description = "Dresses",
+                },
+                new Category
+                {
+                    Id = 3,
+                    Name = "Grocery",
+                    Description = "Grocery Items",
+                }
+            );
+        }
+
+    }
+}
